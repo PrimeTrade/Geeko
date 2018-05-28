b772e97b41669219b754c7db96540e72cb521a51
Nainakshi <nainakshi@primetrade.ai>
2018-05-28 04:08:38 -0700

    candlecreator create candles based on trade batches

--- candleCreator.js
+++ candleCreator.js
@@ -3,5 +3,19 @@ let moment=require('moment');
 
 let candleCreator = function () {
     a.bindAll(this);
+//Remove fixed date
+    this.threshold = moment("1970-01-01","YYYY-MM-DD");
 
+    //Hold the leftover between fetches
+    this.buckets={
+
+    };
+}
+candleCreator.prototype.write=function (batch) {
+    let trade=batch.data;
+    if(a.isEmpty(trade))
+        return;
+    trade=this.filter(trade);
+    this.fillBuckets(trade);
+    
 }
