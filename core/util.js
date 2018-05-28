475f5c4284b2db5ff1f70a4e253f2e2a74aeee0b
kushagra-PrimeTrade <kushagra@primeTrade.ai>
2018-05-28 15:51:25 -0700

    die function to stop the code

--- core/util.js
+++ core/util.js
@@ -117,14 +117,17 @@ function gekkoMode() {
       return 'realtime';
 }
 
+//to call the inherits function of util module
 function inherit(dest, source) {
     require('util').inherits(dest,source);
 }
-  
+ 
+//to call the inherit function of this util file 
 function makeEventEmitter(dest) {
     util.inherit(dest, require('events').EventEmitter);
 }
 
+//function to exit the code with displaying the error
 function die(m, soft) {
     if(_gekkoEnv === 'standalone' || !_gekkoEnv)
       var logs = console.log.bind(console);
@@ -133,17 +136,39 @@ function die(m, soft) {
 
     if(m) {
       if(soft) {
-        logs('\n ERROR: ' + m + '\n\n');
+        logs('ERROR: ' + m + '\n');
       } else {
-        logs('\n\nGekko encountered an error and can\'t continue');
+        logs('\nGekko encountered an error and can\'t continue');
         logs('\nError:\n');
-        logs(m, '\n\n');
-        logs('\nMeta debug info:\n');
+        logs(m, '\n');
+        logs('\nMeta debug info:');
         logs(util.logVersion());
         logs('');
       }
     }
     process.exit(1);
 }
+
+function dir() {
+    let root = __dirname + '/../';
+
+    return {
+      gekko: root,
+      core: root + 'core/',
+      markets: root + 'core/markets/',
+      exchanges: root + 'exchanges/',
+      plugins: root + 'plugins/',
+      methods: root + 'strategies/',
+      indicators: root + 'strategies/indicators/',
+      budfox: root + 'core/budfox/',
+      importers: root + 'importers/exchanges/',
+      tools: root + 'core/tools/',
+      workers: root + 'core/workers/',
+      web: root + 'web/',
+      config: root + 'config/'
+    }
+}
+
+
 
 //console.log(gekkoMode());
