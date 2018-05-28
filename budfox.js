c5bc6a7449bd4648ef8dd48308e6173abe7ab35f
Nainakshi <coder@primetrade.ai>
2018-05-28 02:48:01 -0700

    Initial commit

---
+++ budfox.js
@@ -0,0 +1,12 @@
+//Budfox is small part of gekko's core
+//It aggregates realtime market data from any supported exchange into readable stream of candles
+
+let a = require('lodash');
+let asynch = require('async');
+
+let budfox = function (config) {
+    a.bindAll(this);
+Readable.call( this,{objectMode:true});
+
+}
+let Reabable = require('stream').Readable;
