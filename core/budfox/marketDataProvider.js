c90105303202d28c0aacbe841246e453153574f8
Nainakshi <nainakshi@primetrade.ai>
2018-05-29 03:47:49 -0700

    Emit trades

--- core/budfox/marketDataProvider.js
+++ core/budfox/marketDataProvider.js
@@ -11,3 +11,20 @@ const Manager = function (config) {
     //relay newly fetched trades
     this.source.on('trades batch',this.relayTrades);
 }
+
+util.makeEventEmitter(Manager);
+//Handlers
+
+Manager.prototype.retrieve = function () {
+    this.source.fetch();
+}
+Manager.prototype.relayTrades = function(batch){
+    this.emit('trades',batch);
+    this.sendStartAt(batch);
+    cp.update(batch.last.date.format());
+}
+Manager.prototype.sendStartAt = a.once(function (batch) {
+    cp.startAt(batch.first.date.format())
+
+});
+
