5ca26e4b56379c1815cf447d306ff9b45d2615bd
Nainakshi <nainakshi@primetrade.ai>
2018-05-29 02:29:53 -0700

    The heart emits every 20 seconds

--- core/budfox/heart.js
+++ core/budfox/heart.js
@@ -1,8 +1,8 @@
-let util = require('util');
+let util = require(_dirname + 'util');
 let a= require('lodash');
 let moment = require('moment');
 
-if(util.getConfig().watch.tickrate)
+if( util.getConfig().watch.tickrate)
     let TICKRATING = util.getConfig().watch.tickrate;
 else if(util.getConfig().watch.exchange === 'okcoin')
     let TICKRATING = 2;
@@ -14,3 +14,23 @@ let Heart = function () {
 
     a.bindAll(this);
 }
+
+util.makeEventEmitter(Heart);
+
+Heart.prototype.pump = function () {
+    if(this.lastTick){
+
+        if(this.lastTick < moment().unix() - TICKRATING*3)
+            util.die('Failed to tick in time',true);
+    }
+    this.lastTick = moment().unix();
+    this.emit('tick');
+}
+Heart.prototype.scheduleTicks = function () {
+    setInterval(
+        this.tick,
+        +moment.duration(TICKRATING, 'sec')
+    );
+    a.defer(this.tick);
+}
+
