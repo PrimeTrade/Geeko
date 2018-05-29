337510238852f7b9d3127b83f147dce7b6174042
kushagra-PrimeTrade <coder@primetrade.ai>
2018-05-29 13:55:58 -0700

    function to check array and divisibility

--- core/candleBacher.js
+++ core/candleBacher.js
@@ -14,3 +14,23 @@ function checkSize(candleSize) {
 }
 
 checkSize(10);
+
+
+//function to check if array is passed then add the element of array to smallCandles
+Candle.prototype.write = function(candles) {
+  if(!lodash.isArray(candles))
+     console.log('candles is not an array');
+
+  lodash.each(candles, function(candle) {
+    this.smallCandles.push(candle);
+  }, this);
+}
+
+//check if smallCandle size is divisible by candleSize 
+Candle.prototype.check = function() {
+  if(lodash.size(this.smallCandles) % this.candleSize !== 0)
+    return;
+
+  this.emit('candle', this.calculate());
+  this.smallCandles = [];
+}
