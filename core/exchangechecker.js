011070396f54f43f1aa33f0a5a2ce282ef971a7f
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-05-29 15:32:40 -0700

    function for who cant trade

--- core/exchangechecker.js
+++ core/exchangechecker.js
@@ -6,7 +6,7 @@ let Checker = function() {
 
 Checker.prototype.notValid = function(conf) {
   if(conf.tradingEnabled)
-    return ;//this.cantTrade(conf);
+    return this.cantTrade(conf);
   else
     return this.cantMonitor(conf);
 }
@@ -41,3 +41,32 @@ Checker.prototype.cantMonitor = function(conf) {
   // everyting okay
   return false;
 }
+
+Checker.prototype.cantTrade = function(conf) {
+  let cantMonitor = this.cantMonitor(conf);
+  if(cantMonitor)
+    return cantMonitor;
+
+  let slug = conf.exchange.toLowerCase();
+  let exchange = this.getExchangeCapabilities(slug);
+  let name = exchange.name;
+
+  if(!exchange.tradable)
+    return 'At this moment Gekko can\'t trade at ' + name + '.';
+
+  if(conf.key === 'your-key')
+    return '"your-key" is not a valid API key';
+
+  if(conf.secret === 'your-secret')
+    return '"your-secret" is not a valid API secret';
+
+  let error = false;
+  _.each(exchange.requires, function(req) {
+    if(!conf[req])
+      error = name + ' requires "' + req + '" to be set in the config';
+  }, this);
+
+  return error;
+}
+
+
