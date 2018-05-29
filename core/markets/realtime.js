eb97635dbdaf3a760209bb2f9e59d289b68538e7
kunal-PrimeTrade <kunal@primetrade.ai>
2018-05-29 16:49:18 +0530

    exchngeChecker error handling

--- core/markets/realtime.js
+++ core/markets/realtime.js
@@ -9,3 +9,11 @@ let exchange = _.find(exchanges, (e)=>{
 
 if(!exchange)
     util.die(`Unsupported exchange: ${config.watch.exchange.toLowerCase()}`)
+
+let exchangeChecker = require(util.dirs().core + 'exchangeChecker');
+
+let error = exchangeChecker.cantMonitor(config.watch);
+if(error)
+    util.die(error,true);
+
+module.exports = require(dirs.budfox + 'budfox');
