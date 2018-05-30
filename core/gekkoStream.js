41c8e1fbcb308c460cc7d0674a4a0a3be37230c7
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-05-30 16:22:33 -0700

    write finalize and shutdown the stream

--- core/gekkoStream.js
+++ core/gekkoStream.js
@@ -6,23 +6,47 @@ let util = require('./util');
 
 //initialize the candlecustomer variable and writable stream
 let Gekko = function(candleConsumers) {
-  this.candleConsumers = candleConsumers;
+	this.candleConsumers = candleConsumers;
   
-  Writable.call(this, {objectMode: true});
+  	Writable.call(this, {objectMode: true});
 
-  this.finalize = lodash.bind(this.finalize, this);
+  	this.finalize = lodash.bind(this.finalize, this);
 }
 
 Gekko.prototype = Object.create(Writable.prototype, {
-  constructor: { value: Gekko }
+  	constructor: { value: Gekko }
 });
 
 
 Gekko.prototype.lodashwrite = function(chunk, encoding, lodashdone) {
-  let done = lodash.after(this.candleConsumers.length, lodashdone);
-  lodash.each(this.candleConsumers, function(c) {
-    c.processCandle(chunk, done);
-  });
+  	let done = lodash.after(this.candleConsumers.length, lodashdone);
+  	lodash.each(this.candleConsumers, function(c) {
+    	c.processCandle(chunk, done);
+  	});
 }
+
+Gekko.prototype.finalize = function() {
+  	var tradingMethod = _.find(this.candleConsumers,c => c.meta.name === 'Trading Advisor');
+
+  	if(!tradingMethod)
+    	return this.shutdown();
+
+  	tradingMethod.finish(this.shutdown.bind(this));
+}
+
+Gekko.prototype.shutdown = function() {
+  	async.eachSeries(this.candleConsumers,function(c, callback) {
+      	if (!c.finalize) c.finalize(callback);
+      		callback();
+    },
+    function() {
+      if (env === 'child-process') 
+      		process.send('done');
+      else 
+      		process.exit(0);
+    });
+};
+
+module.exports = Gekko;
 
 
