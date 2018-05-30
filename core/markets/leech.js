064ed22351fe607f9a5ab27501494d979e8af251
kunal-PrimeTrade <kunal@primetrade.ai>
2018-05-30 15:57:55 +0530

    Validating exchanges for the market

--- core/markets/leech.js
+++ core/markets/leech.js
@@ -4,7 +4,20 @@ const util=require('../util');
 const config=util.getConfig();
 const dirs=util.dirs();
 const moment=require('moment');
+const exchangeChecker = require(dirs.core + 'exchangeChecker');
+const adapter = config[config.adapter];
+const Reader = require(dirs.gekko + adapter.path + '/reader');
+const TICKINTERVAL = 20*1000; //20 sec
+const slug = config.watch.exchange.toLowerCase();
+const exchange = exchangeChecker.getExchangeCapabilities(slug);
 let fromTs;
+
+if(!exchange)
+    util.die(`Exchange Unsupported: ${slug}`);
+
+const error = exchangeChecker.cantMonitor(config.watch);
+if(error)
+    util.die(error, true);
 
 if(config.market.from)
     fromTs = moment.utc(config.market.from).unix();
