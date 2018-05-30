aa9c499a5ec2c4f590c60622bf7eacdc7f383c3d
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-05-30 13:27:26 -0700

    changes n util.js file

--- core/util.js
+++ core/util.js
@@ -15,169 +15,178 @@ let _args = false;
 let startTime = moment;
 //helper functions
 
-//check for 2 instances if same or not
-//return true when same false when not same
-function equals(a,b){
-	if(a==b)
-		return true;
-	else
-		return false;
-}
 
-//function to converts minutes to MM
-function minToMs(min){
-	return min*60*1000;
-}
+var util = {
 
-//set the config status
-function setConfig(config){
-	_config = config;
-}
+	//check for 2 instances if same or not
+	//return true when same false when not same
+ 	equals: function(a,b){
+		if(a==b)
+			return true;
+		else
+			return false;
+	},
 
-//to get the configuration of Gekko
-function getConfig() {
+	//function to converts minutes to MM
+	minToMs: function(min){
+		return min*60*1000;
+	},
 
-    if(_config)
-      return _config;
+	//set the config status
+	setConfig: function(config){
+		_config = config;
+	},
 
-    if(!program.config)
-        util.die('Please specify a config file.', true);
+	//to get the configuration of Gekko
+	getConfig: function() {
 
-    if(!fs.existsSync(util.dirs().gekko + program.config))
-        util.die('Cannot find the specified config file.', true);
+    	if(_config)
+      		return _config;
 
-    _config = require(util.dirs().gekko + program.config);
+    	if(!program.config)
+        	util.die('Please specify a config file.', true);
+
+    	if(!fs.existsSync(util.dirs().gekko + program.config))
+        	util.die('Cannot find the specified config file.', true);
+
+    	_config = require(util.dirs().gekko + program.config);
     return _config;
-}
+	},
 
-//return the current package
-function getPackage(){
-	if(_package)
-	{
-		return _package;
-	}
+	//return the current package
+	getPackage: function(){
+		if(_package)
+		{	
+			return _package;
+		}
 	//_package = JSON.parse( fs.readFileSync(__dirname + '/../package.json', 'utf8') );
 	return _package;
-}
+	},
 
-//function to get the starttime
-function getStartTime(){
-	return startTime;
-}
+	//function to get the starttime
+	getStartTime: function(){
+		return startTime;
+	},
 
-//function for UI
-function launchUI(){
-	if(program['ui'])
-      return true;
+	//function for UI
+	launchUI: function(){
+		if(program['ui'])
+      		return true;
     return false;
-}
+	},
 
-//function to set the gekkoEnc variable
-function setGekkoEnv(env) {
-	_gekkoEnv = env;
-}
+	//function to set the gekkoEnc variable
+	setGekkoEnv: function(env) {
+		_gekkoEnv = env;
+	},
   
-//function to get gekToEnv
-function gekkoEnv() {
-    return _gekkoEnv || 'standalone';
-}
+	//function to get gekToEnv
+	gekkoEnv: function() {
+    	return _gekkoEnv || 'standalone';
+	},
 
-//function to call retryHelper according to the parameter
-function retryCustom(options, fn, callback) {
-    retryHelper(fn, options, callback);
-}
+	//function to call retryHelper according to the parameter
+	retryCustom: function(options, fn, callback) {
+    	retryHelper(fn, options, callback);
+	},
 
-//function to call retryHelper setting maxTimeout and minTimeout
-function retry1(fn, callback) {
-    let options = {
-      retries: 5,
-      factor: 1.2,
-      minTimeout: 1 * 1000,
-      maxTimeout: 3 * 1000
-    };
-    retryHelper(fn, options, callback);
-}
+	//function to call retryHelper setting maxTimeout and minTimeout
+	retry1: function(fn, callback) {
+    	let options = {
+      		retries: 5,
+      		factor: 1.2,
+      		minTimeout: 1 * 1000,
+      		maxTimeout: 3 * 1000
+    	};
+    	retryHelper(fn, options, callback);
+	},
 
-//function to declare all three modes of gekko
-function gekkoModes() {
-	let a=['importer','backtest','realtime']
-    return a;
-}
+	//function to declare all three modes of gekko
+	gekkoModes: function() {
+		let a=['importer','backtest','realtime']
+    	return a;
+	},
 
-//check the mode of gekko if gekkomode is set
-function gekkoMode() {
-    if(_gekkoMode)
-      return _gekkoMode;
+	//check the mode of gekko if gekkomode is set
+	gekkoMode: function() {
+    	if(_gekkoMode)
+      		return _gekkoMode;
 
-    if(program.importer)
-      return 'importer';
-    else if(program.backtest)
-      return 'backtest';
-    else if(program.realtime)   
-      return 'realtime';
-}
+    	if(program.importer)
+      		return 'importer';
+    	else if(program.backtest)
+      		return 'backtest';
+    	else if(program.realtime)   
+      		return 'realtime';
+	},
 
-//to call the inherits function of util module
-function inherit(dest, source) {
-    require('util').inherits(dest,source);
-}
+	//to call the inherits function of util module
+	inherit: function(dest, source) {
+    	require('util').inherits(dest,source);
+	},
  
-//to call the inherit function of this util file 
-function makeEventEmitter(dest) {
-    util.inherit(dest, require('events').EventEmitter);
+	//to call the inherit function of this util file 
+	makeEventEmitter: function(dest) {
+    	util.inherit(dest, require('events').EventEmitter);
+	},
+
+	//function to exit the code with displaying the error
+	die: function(m, soft) {
+    	if(_gekkoEnv === 'standalone' || !_gekkoEnv)
+      		var logs = console.log.bind(console);
+    	else if(_gekkoEnv === 'child-process')
+      		var logs = m => process.send({type: 'error', error: m});
+
+    	if(m) {
+      		if(soft) {
+        		logs('ERROR: ' + m + '\n');
+      		} 
+      		else {
+        		logs('\nGekko encountered an error and can\'t continue');
+        		logs('\nError:\n');
+        		logs(m, '\n');
+        		logs('\nMeta debug info:');
+        		logs(util.logVersion());
+        		logs('');
+      		}
+    	}
+    	process.exit(1);
+	},
+
+	//function for dir
+	dir: function() {
+    	let root = __dirname + '/../';
+
+    	return {
+      		gekko: root,
+      		core: root + 'core/',
+      		markets: root + 'core/markets/',
+      		exchanges: root + 'exchanges/',
+      		plugins: root + 'plugins/',
+      		methods: root + 'strategies/',
+      		indicators: root + 'strategies/indicators/',
+     	 	budfox: root + 'core/budfox/',
+      		importers: root + 'importers/exchanges/',
+      		tools: root + 'core/tools/',
+      		workers: root + 'core/workers/',
+      		web: root + 'web/',
+      		config: root + 'config/'
+    	}
+	},
+
+	//function to get the node version
+	getRequiredNodeVersion: function() {
+    	return util.getPackage().engines.node;
+	},	
+
+	recentNode: function(){
+    	let required = util.getRequiredNodeVersion();
+    	return semver.satisfies(process.version, required);
+	},
+	
 }
 
-//function to exit the code with displaying the error
-function die(m, soft) {
-    if(_gekkoEnv === 'standalone' || !_gekkoEnv)
-      var logs = console.log.bind(console);
-    else if(_gekkoEnv === 'child-process')
-      var logs = m => process.send({type: 'error', error: m});
 
-    if(m) {
-      if(soft) {
-        logs('ERROR: ' + m + '\n');
-      } else {
-        logs('\nGekko encountered an error and can\'t continue');
-        logs('\nError:\n');
-        logs(m, '\n');
-        logs('\nMeta debug info:');
-        logs(util.logVersion());
-        logs('');
-      }
-    }
-    process.exit(1);
-}
-
-function dir() {
-    let root = __dirname + '/../';
-
-    return {
-      gekko: root,
-      core: root + 'core/',
-      markets: root + 'core/markets/',
-      exchanges: root + 'exchanges/',
-      plugins: root + 'plugins/',
-      methods: root + 'strategies/',
-      indicators: root + 'strategies/indicators/',
-      budfox: root + 'core/budfox/',
-      importers: root + 'importers/exchanges/',
-      tools: root + 'core/tools/',
-      workers: root + 'core/workers/',
-      web: root + 'web/',
-      config: root + 'config/'
-    }
-}
-
-//function to get the node version
-function getRequiredNodeVersion() {
-    return util.getPackage().engines.node;
-}
-
-function recentNode(){
-    let required = util.getRequiredNodeVersion();
-    return semver.satisfies(process.version, required);
-}
 
 let retryHelper = function(fn, options, callback) {
   let operation = retry.operation(options);
@@ -205,5 +214,5 @@ if(!util.recentNode())
   ].join(''), true);
 }
 
-//module.exports = util;
+module.exports = util;
 //console.log(gekkoMode());
