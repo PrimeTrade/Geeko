261e7b7ea90a48bba7f3280a8c39d6fbcd58efd4
Nainakshi <nainakshi@primetrade.ai>
2018-05-30 01:16:20 -0700

    remove trades that have zero amount

--- core/budfox/tradeBatcher.js
+++ core/budfox/tradeBatcher.js
@@ -13,13 +13,13 @@ let tradeBatcher = function (tid) {
 }
 util.makeEventEmitter(tradeBatcher);
 tradeBatcher.prototype.write = function (batch) {
-    if(!a.isArray(batch))
+    if (!a.isArray(batch))
         throw 'batch is not an array';
-    if(a.isEmpty(batch))
+    if (a.isEmpty(batch))
         return log.debug('Trade fetch came back empty');
     let filterBatch = this.filter(batch);
     let amount = a.size(filterBatch);
-    if(!amount)
+    if (!amount)
         return log.debug('No new trades');
 
     let momentBatch = this.convertDates(filterBatch);
@@ -28,12 +28,46 @@ tradeBatcher.prototype.write = function (batch) {
 
 
     log.debug(
-        'Processing',amount,'new trades',
+        'Processing', amount, 'new trades',
         'From',
         firstone.date.format('YYYY-MM-DD HH:mm:ss'),
         'UTC to',
         lastone.date.format('YYYY-MM-DD HH:mm:ss'),
         'UTC',
-        '(' +first.date.from(lastone.date,true) + ')'
+        '(' + first.date.from(lastone.date, true) + ')'
     );
+
+    this.emit('new batch', {
+        amount: amount,
+        start: firstone.date,
+        end: lastone.date,
+        last: lastone,
+        first: firstone,
+        data: momentBatch
+
+    });
+    this.last = last[this.tid];
+    if (this.tid === 'date')
+        this.last = this.last.unix;
+}
+
+tradeBatcher.prototype.filter = function (batch) {
+    //Make sure we are not trying to count beyond infinity
+    let lastTid = a.last(batch)[this.tid];
+    if(lastTid === lastTid+1)
+        util.die('trade tid is max int');
+    //remove trade that have zero amount
+    batch = a.filter(batch,function (trade) {
+        return this.last < trade[this.tid];
+
+    },this );
+}
+tradeBatcher.prototype.convertDates = function (batch) {
+    return a.map(a.cloneDeep(batch),function (trade) {
+        trade.date = moment.unix(trade.date).utc();
+        return trade;
+    });
+}
+module.exports = tradeBatcher;
+
 }
