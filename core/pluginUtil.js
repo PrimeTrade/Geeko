50d6cd8235eed6d5a78612ef71bcdf588cf9e69c
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-05-30 12:55:20 -0700

    exporting the file

--- core/pluginUtil.js
+++ core/pluginUtil.js
@@ -1,7 +1,15 @@
+//file to configure plugins and check if they can be loaded or not
 let lodash = require("lodash");
 let util = require("./util");
 
 let error, constructor;
+
+let log = require(util.dirs().core + 'log');
+//get configuration dirs plugin an the three different gekkomodes
+let config = util.getConfig();
+let pluginDir = util.dirs().plugins;
+lets gekkoMode = util.gekkoMode();
+//three geeko modes:- RealTime, BackTesting, importer
 
 let pluginHelper = { 
 
@@ -39,14 +47,16 @@ let pluginHelper = {
 		return error;
   	},
   	
-  	
+  	//function when the plugin can be loaded
   	load: function(plugin, next) {
 
+		//the configuration of the plugin
     	plugin.config = config[plugin.slug];
 
     	if(!plugin.config || !plugin.config.enabled)
       		return next();
 
+		//mode enetered which is not suppored by the geeko
     	if(!_.contains(plugin.modes, gekkoMode)) {
       		log.warn('The plugin',plugin.name,'does not support the mode',gekkoMode + '.','It has been disabled.')
       		return next();
@@ -65,6 +75,28 @@ let pluginHelper = {
       		Constructor = require(pluginDir + plugin.path(config));
     	else
       		Constructor = require(pluginDir + plugin.slug);
+      		
+      	if(plugin.async) {
+      		let instance = new Constructor(util.defer(function(err) {
+        		next(err, instance);
+      		}), plugin);
+      
+      	instance.meta = plugin;
+    	} 
+    	else 
+    	{
+      		let instance = new Constructor(plugin);
+      		instance.meta = plugin;
+      		lodash.defer(function() {
+        		next(null, instance); 
+      		});
+    	}
+
+    	if(!plugin.silent)
+      		log.info('\n');
   	},
 
 }
+
+//export the two functions
+module.exports = pluginHelper;
