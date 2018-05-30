de99f3ac6ce09bed59db16794c9a927547022683
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-05-30 12:27:09 -0700

    function for load

--- core/pluginUtil.js
+++ core/pluginUtil.js
@@ -1,5 +1,7 @@
 let lodash = require("lodash");
-let util = require("/util");
+let util = require("./util");
+
+let error, constructor;
 
 let pluginHelper = { 
 
@@ -7,26 +9,62 @@ let pluginHelper = {
 
 //helper function to check for module that can't be load
 
-	if(lodash.has(plugin, 'dependencies'))
-      	let error = false;
+	cannotLoad: function(plugin) {
 
-    lodash.each(plugin.dependencies, function(dep) {
-        try {
-          let a = require(dep.module);
-        }
-        catch(e) {
-          log.error('ERROR LOADING DEPENDENCY', dep.module);
+		if(lodash.has(plugin, 'dependencies'))
+      		error = false;
 
-          if(!e.message) {
-            log.error(e);
-            util.die();
-          }
+    	lodash.each(plugin.dependencies, function(dep) {
+        	try {
+          		let a = require(dep.module);
+        	}
+        	catch(e) {
+          		log.error('ERROR LOADING DEPENDENCY', dep.module);
 
-          if(!e.message.startsWith('Cannot find module'))
-            return util.die(e);
-        }
-      });
+          		if(!e.message) {
+            		log.error(e);
+            		util.die();
+          		}
 
-    return error;
-  },
-  
+          		if(!e.message.startsWith('Cannot find module'))
+            		return util.die(e);
+        		
+        		if(!e.message.startsWith('Cannot find module'))
+            		return util.die(e);
+
+          		error = ['The plugin',plugin.slug,'expects the module',dep.module,'to be installed.','However it is not, install','it by running: \n\n','\tnpm install',dep.module + '@' + dep.version].join(' ');
+          	}
+      	});
+	
+		return error;
+  	},
+  	
+  	
+  	load: function(plugin, next) {
+
+    	plugin.config = config[plugin.slug];
+
+    	if(!plugin.config || !plugin.config.enabled)
+      		return next();
+
+    	if(!_.contains(plugin.modes, gekkoMode)) {
+      		log.warn('The plugin',plugin.name,'does not support the mode',gekkoMode + '.','It has been disabled.')
+      		return next();
+    	}
+
+    	log.info('Setting up:');
+    	log.info('\t', plugin.name);
+    	log.info('\t', plugin.description);
+
+    	let cannotLoad = pluginHelper.cannotLoad(plugin);
+    	
+    	if(cannotLoad)
+      		return next(cannotLoad);
+
+    	if(plugin.path)
+      		Constructor = require(pluginDir + plugin.path(config));
+    	else
+      		Constructor = require(pluginDir + plugin.slug);
+  	},
+
+}
