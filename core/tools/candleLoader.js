52d74ca491c710d56f3ba0c3fc5a94916402edc2
Nainakshi <nainakshi@primetrade.ai>
2018-05-31 00:31:40 -0700

    Handle Daterange

---
+++ core/tools/candleLoader.js
@@ -0,0 +1,58 @@
+const batchSize = 1000;
+
+const _ = require('lodash');
+const fs = require('fs');
+const moment = require('moment');
+
+const util = require('../../core/util');
+const config = util.getConfig();
+const dirs = util.dirs();
+const log = require(dirs.core + '/log');
+
+const adapter = config[config.adapter];
+const Reader = require(dirs.geeko + adapter.path + '/reader');
+const daterange = config.daterange;
+
+const candleBatcher = require(dirs.core + 'candleBatcher');
+
+const to = moment.utc(daterange.to).startOf('minute');
+const from = moment.utc(daterange.from).startOf('minute');
+const toUnix = to.unix();
+
+if(to <= from)
+util.die('Daterange does not make sense');
+
+if(!from.isValid())
+    util.die('invalid `from` ');
+
+if(!to.isValid())
+    util.die('invalid `to` ');
+
+let iterator = {
+    from: from.clone(),
+    to: from.clone().add(batchSize, 'min').subtract(1, 'sec')
+}
+
+let DONE = false;
+let result = [];
+let reader = new Reader();
+let batcher;
+let next;
+
+let doneFn = () => {
+    process.nextTick(() => {
+        next(result);
+    })
+};
+
+module.exports = function (candleSize,next) {
+    next = _.once(_next);
+
+
+    batcher= new candleBatcher(candleSize).on('candle', handleBatchedCandles);
+    getBatch();
+}
+
+
+}
+
