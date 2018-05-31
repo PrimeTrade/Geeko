560f5a3aa5166d45df6ca437e84b919df8d47e0b
kunal-PrimeTrade <kunal@primetrade.ai>
2018-05-31 17:29:35 +0530

    Handling client messages for backtesting

---
+++ core/workers/pipeline/messageHandlers/backtestHandler.js
@@ -0,0 +1,31 @@
+module.exports =done=>{
+    let trades =[];
+    let roundtrips = [];
+    let candles = [];
+    let report = false;
+    // listen to all messages and internally queue all candles and trades
+
+    return{
+        message: msg=>{
+
+            if(msg.type === 'candle')
+                candles.push(message.candle);
+            else if(msg.type === 'trade')
+                trades.push(message.roundtrip);
+            else if(msg.type === 'roundtrip')
+                roundtrips.push(message.roundtrip);
+            else if(msg.type === 'report')
+                report = msg.report;
+            else if(msg.log)
+                console.log(msg.log);
+        },
+        exit: status => {
+            if(status !== 0)
+                done('child process has died');
+            else
+                done(null, {
+                    trades: trades, candles: candles, report: report, roundtrips: roundtrips
+                });
+        }
+    }
+}
