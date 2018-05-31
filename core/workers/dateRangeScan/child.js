458b867ffbdbdf6b947a1a3e81caf00df656c316
kunal-PrimeTrade <kunal@primetrade.ai>
2018-05-31 14:41:26 +0530

    Disable debug and persisting configurations for date range scan

---
+++ core/workers/dateRangeScan/child.js
@@ -0,0 +1,23 @@
+let util = require(__dirname + '../../util');
+let dirs = util.dirs();
+let ipc = require('relieve').IPCEE(process);
+
+ipc.on('start', config=>{
+    //correct geeko env forcefully
+    util.setGekkoEnv('child-process');
+
+    //force disable debug
+    config.debug = false;
+
+    //persist config
+    util.setConfig(config);
+
+    var let scan = require(dirs.tools + 'dateRangeScanner');
+    scan(
+        (error, ranges, reader)=>{
+            reader.close();
+            ipc.send('ranges', ranges);
+            process.exit(0);
+        }
+    );
+});
