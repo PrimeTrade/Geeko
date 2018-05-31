8124d345e9beecc0a2993f76b5fa6bfcc585431b
kunal-PrimeTrade <kunal@primetrade.ai>
2018-05-31 15:38:34 +0530

    Disabling debug and then loading candles for parent process

---
+++ core/workers/loadCandles/child.js
@@ -0,0 +1,22 @@
+let start = (config, candleSize, daterange)=>{
+    let util = require('/../../util');
+
+    //force correct geeko env
+    util.setGekkoEnv('child-process');
+
+    //force disable debug
+    util.debug = false;
+    util.setConfig(config);
+
+    let dirs = util.dirs();
+    let load = require(dirs.tools + 'candleLoader');
+    load(config.candleSize, candles=>{
+        process.send(candles);
+    })
+}
+process.send('ready');
+
+process.on('message', (m)=>{
+    if(m.what === 'start')
+        start(m.config, m.candleSize, m.daterange);
+});
