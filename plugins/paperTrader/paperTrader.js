bd561f22b1802bd094f329139259aec2f8a3b537
Nainakshi <nainakshi@primetrade.ai>
2018-06-02 11:26:06 -0700

    Calculates Gekko's profit in %

--- plugins/paperTrader/paperTrader.js
+++ plugins/paperTrader/paperTrader.js
@@ -54,4 +54,38 @@ return amount;
 PaperTrade.prototype.setStartBalance = function(){
 this.portfolio.balance = this.portfolio.currency + this.price * this.portfolio.asset;
 this.relayPortfolio();
+
+
+PaperTrader.prototype.updatePosition = function(advice){
+let what = advice.recommendation;
+let price = advice.candle.close;
 }
+if(what === 'long') {
+    this.portfolio.asset += this.extractFee(this.portfolio.currency / price);
+    this.portfolio.currency = 0;
+    this.trades++;
+}
+else if(what === 'short') {
+    this.portfolio.currency += this.extractFee(this.portfolio.asset * price);
+    this.portfolio.asset = 0;
+    this.trades++;
+}
+}
+PaperTrader.prototype.processAdvice = function(advice) {
+    if(advice.recommendation === 'soft')
+        return;
+
+    this.updatePosition(advice);
+    this.relayTrade(advice);
+}
+
+PaperTrader.prototype.processCandle = function(candle, done) {
+    this.price = candle.close;
+
+    if(!this.portfolio.balance)
+        this.setStartBalance();
+
+    done();
+}
+
+module.exports = PaperTrader;
