03351ea5f681ea197b8051a3f1b51b579b926f33
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-06-03 15:35:54 -0700

    buy, sell and chech the order deatils for poloniex exchange

--- exchanges/poloniex.js
+++ exchanges/poloniex.js
@@ -81,3 +81,76 @@ trader.prototype.getPortfolio = function(callback) {
 
 	this.poloniex.myBalances(set);
 }
+
+trader.prototype.getTicker = function(callback) {
+	let args = lodash.toArray(arguments);
+	this.poloniex.getTicker(function(err, data) {
+		if(err)
+			return this.retry(this.getTicker, args);
+
+		let tick = data[this.pair];
+
+		callback(null, {
+			bid: parseFloat(tick.highestBid),
+			ask: parseFloat(tick.lowestAsk),
+		});
+
+	}.bind(this));
+}
+
+trader.prototype.getFee = function(callback) {
+	let set = function(err, data) {
+		if(err || data.error)
+			return callback(err || data.error);
+
+		callback(false, parseFloat(data.makerFee));
+	}
+	this.poloniex.lodashprivate('returnFeeInfo', lodash.bind(set, this));
+}
+
+trader.prototype.getLotSize = function(tradeType, amount, price, callback) {
+  if (amount < this.market.minimalOrder.amount)
+    return callback(undefined, { amount: 0, price: 0 });
+
+  if (amount * price < this.market.minimalOrder.order)
+    return callback(undefined, { amount: 0, price: 0});
+
+  callback(undefined, { amount: amount, price: price });
+}
+
+trader.prototype.buy = function(amount, price, callback) {
+	let args = lodash.toArray(arguments);
+	let set = function(err, result) {
+		if(err || result.error) {
+			log.error('unable to buy:', err, result);
+			return this.retry(this.buy, args);
+		}
+
+		callback(null, result.orderNumber);
+	}.bind(this);
+
+	this.poloniex.buy(this.currency, this.asset, price, amount, set);
+}
+
+trader.prototype.sell = function(amount, price, callback) {
+	let args = lodash.toArray(arguments);
+	let set = function(err, result) {
+		if(err || result.error) {
+			log.error('unable to sell:', err, result);
+			return this.retry(this.sell, args);
+		}
+
+		callback(null, result.orderNumber);
+	}.bind(this);
+
+	this.poloniex.sell(this.currency, this.asset, price, amount, set);
+}
+
+trader.prototype.checkOrder = function(order, callback) {
+	let check = function(err, result) {
+		let stillThere = lodash.find(result, function(o) { return o.orderNumber === order });
+		callback(err, !stillThere);
+	}.bind(this);
+
+	this.poloniex.myOpenOrders(this.currency, this.asset, check);
+}
