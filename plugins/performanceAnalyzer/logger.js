64f3accbfcbe93709c045e80aed607500d20adf9
Nainakshi <nainakshi@primetrade.ai>
2018-06-03 10:46:02 -0700

    used for realtime logging and backtest logging

--- plugins/performanceAnalyzer/logger.js
+++ plugins/performanceAnalyzer/logger.js
@@ -21,4 +21,30 @@ Logger.prototype.round = function(amount) {
 }
 
 Logger.prototype.handleStartBalance = function() {
+//noop
 }
+Logger.prototype.logReport = function (trade,report) {
+    let start = this.round(report.startBalance);
+    let current = this.round(report.balance);
+    log.info(`(PROFIT REPORT) original simulated balance:\t ${start} ${this.currency} `);
+    log.info(`(PROFIT REPORT) current simulated balance:\t ${current} ${this.currency}`);
+    log.info(
+        `(PROFIT REPORT) simulated profit:\t\t ${this.round(report.profit)} ${this.currency}`,
+        `(${this.round(report.relativeProfit)}%)`
+    );
+}
+Logger.prototype.logRoundtripHeading = function () {
+    log.info('(ROUNDTRIP)', 'entry date (UTC)  \texit date (UTC)  \texposed duration\tP&L \tprofit');
+}
+Logger.prototype.logRoundtrip = function (rt) {
+    const display = [
+        rt.entryAt.utc().format('YYYY-MM-DD HH:mm'),
+        rt.exitAt.utc().format('YYYY-MM-DD HH:mm'),
+        (moment.duration(rt.duration).humanize() + "   ").slice(0,16),
+        rt.pnl.toFixed(2),
+        rt.profit.toFixed(2)
+    ];
+
+    log.info('(ROUNDTRIP)', display.join('\t'));
+}
+
