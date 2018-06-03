27de45a7d6fc67ee56f6fe8616de9e8e7db3c412
kunal-PrimeTrade <kunal@primetrade.ai>
2018-06-03 22:59:44 +0530

    buying orders for cexio exchange

--- exchanges/cexio.js
+++ exchanges/cexio.js
@@ -60,3 +60,65 @@ trader.prototype.buy = (amount, price, callback)=> {
 
     this.cexio.place_order('buy', amount, price, _.bind(set, this));
 };
+
+trader.prototype.sell = (amount, price, callback)=> {
+    // Prevent "You incorrectly entered one of fields."
+    // because of more than 8 decimals.
+    amount *= 100000000;
+    amount = Math.floor(amount);
+    amount /= 100000000;
+
+    // test placing orders which will not be filled
+    //price *= 10; price = price.toFixed(8);
+
+    log.debug('SELL', amount, this.asset, '@', price, this.currency);
+
+    let set = (err, data)=> {
+        if(err)
+            return log.error('unable to sell:', err);
+        if(data.error)
+            return log.error('unable to sell:', data.error);
+
+        log.debug('SELL order placed.  Order ID', data.id);
+        callback(null, data.id);
+    };
+
+    this.cexio.place_order('sell', amount, price, _.bind(set, this));
+};
+
+trader.prototype.retry = (method, args, err)=> {
+    let wait = +moment.duration(10, 'seconds');
+    log.debug(this.name, 'returned an error, retrying..', err, 'waiting for', wait, 'ms');
+
+    if (!_.isFunction(method)) {
+        log.error(this.name, 'failed to retry, no method supplied.');
+        return;
+    }
+
+    let self = this;
+
+    // make sure the callback (and any other fn)
+    // is bound to trader
+    _.each(args, (arg, i)=> {
+        if(_.isFunction(arg))
+            args[i] = _.bind(arg, self);
+    });
+
+    // run the failed method again with the same
+    // arguments after wait
+    setTimeout(
+        function() { method.apply(self, args) },
+        wait
+    );
+};
+
+trader.prototype.getTicker = (callback)=> {
+    let set = (err, data)=> {
+        let ticker = {
+            ask: data.ask,
+            bid: data.bid
+        };
+        callback(err, ticker);
+    };
+    this.cexio.ticker(_.bind(set, this));
+};
