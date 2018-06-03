958855807eb7f791961e960fa0c5a7d9ca7f65ef
kushagra-PrimeTrade <kushagra@primetrade.ai>
2018-06-03 00:55:49 -0700

    handle the fetch details of the different trades

--- importers/exchanges/poloniex.js
+++ importers/exchanges/poloniex.js
@@ -50,3 +50,54 @@ util.makeEventEmitter(Fetcher);
 let iterator = false;
 let end = false;
 let done = false;
+
+
+let fetcher = new Fetcher(config.watch);
+
+let fetch = () => {
+  	log.info(config.watch.currency,config.watch.asset,'Requesting data from',iterator.from.format('YYYY-MM-DD HH:mm:ss') + ',','to',iterator.to.format('YYYY-MM-DD HH:mm:ss'));
+
+  	if(util.gekkoEnv === 'child-process') {
+    	let msg = ['Requesting data from',iterator.from.format('YYYY-MM-DD HH:mm:ss') + ',','to',iterator.to.format('YYYY-MM-DD HH:mm:ss')].join('');
+    	process.send({type: 'log', log: msg});
+  	}
+  	fetcher.getTrades(iterator, handleFetch);
+}
+
+let handleFetch = trades => {
+  	iterator.from.add(batchSize, 'minutes').subtract(overlapSize, 'minutes');
+  	iterator.to.add(batchSize, 'minutes').subtract(overlapSize, 'minutes');
+
+  	if(!lodash.size(trades)) {
+    
+    	if(iterator.to.clone().add(batchSize * 4, 'minutes') > end) {
+      		fetcher.emit('done');
+    	}
+
+    	return fetcher.emit('trades', []);
+  	}
+
+  	let last = moment.unix(lodash.last(trades).date);
+
+  	if(last > end) {
+    	fetcher.emit('done');
+
+    	let endUnix = end.unix();
+    	trades = lodash.filter(trades,t => t.date <= endUnix);
+  	}
+
+  	fetcher.emit('trades', trades);
+}
+
+
+module.exports = function (daterange) {
+	iterator = {
+    	from: daterange.from.clone(),
+    	to: daterange.from.clone().add(batchSize, 'minutes')
+  	}
+  
+  
+	end = daterange.to.clone();
+
+  	return {bus: fetcher,fetch: fetch}
+}
