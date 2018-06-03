dcf6723f17daae6a071159684b6d1269d0d03789
kunal-PrimeTrade <kunal@primetrade.ai>
2018-06-03 17:11:59 +0530

    getting portfolio and charged fee for wex.nz exchange

--- exchanges/wex.nz.js
+++ exchanges/wex.nz.js
@@ -63,7 +63,7 @@ trader.prototype.sell = (amount, price, callback)=> {
     setTimeout(function() {
         this.wex.trade(this.pair, 'sell', price, amount, _.bind(set, this));
     }.bind(this), 1000);
-}
+};
 
 // if the exchange errors we try the same call again after
 // waiting 10 seconds
@@ -79,4 +79,67 @@ trader.prototype.retry = (method, args)=> {
         function() { method.apply(self, args) },
         wait
     );
+};
+
+trader.prototype.getPortfolio = (callback)=> {
+    let args = _.toArray(arguments);
+    let calculate = (err, data)=> {
+        if(err) {
+            if(err.message === 'invalid api key')
+                util.die('Your ' + this.name + ' API keys are invalid');
+            return this.retry(this.getPortfolio, args);
+        }
+        if(_.isEmpty(data))
+            err = 'no data';
+
+        if (err || !data.return || !data.return.funds)
+            return this.retry(this.getPortfolio, args);
+
+        let assetAmount = parseFloat( data.return.funds[this.asset.toLowerCase()] );
+        let currencyAmount = parseFloat( data.return.funds[this.currency.toLowerCase()] );
+
+        if(!_.isNumber(assetAmount) || _.isNaN(assetAmount)) {
+            log.error(`wex.nz did not return portfolio for ${this.asset}, assuming 0.`);
+            assetAmount = 0;
+        }
+
+        if(!_.isNumber(currencyAmount) || _.isNaN(currencyAmount)) {
+            log.error(`wex.nz did not return portfolio for ${this.currency}, assuming 0.`);
+            currencyAmount = 0;
+        }
+
+        let portfolio = [
+            { name: this.asset, amount: assetAmount },
+            { name: this.currency, amount: currencyAmount }
+        ];
+
+        callback(err, portfolio);
+    };
+    calculate.bind(this);
+
+    this.wex.getInfo(calculate);
+};
+
+trader.prototype.getTicker = (callback)=> {
+    // wex.nz doesn't state asks and bids in its
+    // ticker
+    let set = (err, data)=> {
+        if(err)
+            return this.retry(this.wex.ticker, [this.pair, set]);
+
+        let ticker = {
+            ask: data.ticker.buy,
+            bid: data.ticker.sell
+        };
+
+        callback(err, ticker);
+    };
+    set.bind(this);
+    this.wex.ticker(this.pair, set);
+};
+
+trader.prototype.getFee = (callback)=> {
+    // wex.nz doesn't have different fees based on orders
+    // at this moment it is always 0.2%
+    callback(false, 0.002);
 };
