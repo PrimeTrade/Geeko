66ee29d5da445f37ffd62e869749a30723c42fde
Nainakshi <nainakshi@primetrade.ai>
2018-06-03 23:19:53 -0700

    Table exist

--- plugins/postgresql/reader.js
+++ plugins/postgresql/reader.js
@@ -59,4 +59,95 @@ Reader.prototype.mostRecentWindow = function (from,to,next) {
         });
     });
  }
+ Reader.prototype.tableExists = function(name,next){
+     this.db.query(`
+     SELECT table name
+        FROM information_schema.tables
+        WHERE table_schema = ' ${postgresUtil.schema()} '
+        AND table_name = '${postgresUtil.table(name)}';
+        `, function(err,result){
+         if(err){
+             return util.die('At `tableExists` DB error');
+         }
+         next(null,result.rows.length === 1);
+     });
+
+ }
+
+ Reader.prototype.get = function (from,to,what,next) {
+     if(what === 'full'){
+         what = '*';
+     }
+     let query = this.db.query(new Query(`
+     SELECT ${what} from ${postgresUtil.table('candles')}
+     where start <= ${to} AND start >= ${from}
+     ORDER BY start ASC
+     `));
+
+     let rows =[];
+     query.on('row',function (row) {
+         rows.push(row)
+     });
+     query.on('end',function () {
+         next(null,rows);
+     });
+ }
+ Reader.prototype.count = function (from,to,next) {
+     let query = this.db.query(new Query(`
+     SELECT count(*) as count from ${postgresUtil.table('candles')}
+     where start <= ${to} AND start >= ${from}
+     `));
+     let rows = [];
+     query.on('row',function (row) {
+         rows.push(row);
+     });
+     query.on('end',function () {
+         next(null,_.first(rows).count);
+     });
+ }
+Reader.prototype.countTotal = function(next) {
+    let query = this.db.query(new Query(`
+  SELECT COUNT(*) as count from ${postgresUtil.table('candles')}
+  `));
+    let rows = [];
+    query.on('row', function(row) {
+        rows.push(row);
+    });
+
+    query.on('end',function(){
+        next(null, _.first(rows).count);
+    });
+}
+
+Reader.prototype.getBoundry = function(next) {
+    let query = this.db.query(new Query(`
+  SELECT (
+    SELECT start
+    FROM ${postgresUtil.table('candles')}
+    ORDER BY start LIMIT 1
+  ) as first,
+  (
+    SELECT start
+    FROM ${postgresUtil.table('candles')}
+    ORDER BY start DESC
+    LIMIT 1
+  ) as last
+  `));
+    let rows = [];
+    query.on('row', function(row) {
+        rows.push(row);
+    });
+
+    query.on('end',function(){
+        next(null, _.first(rows));
+    });
+}
+
+Reader.prototype.close = function() {
+    this.db.end();
+}
+
+module.exports = Reader;
+
+ 
 
