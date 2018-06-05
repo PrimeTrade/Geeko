535afed9dcd7a03487d7ed00123d0752d2ee5f04
Nainakshi <nainakshi@primetrade.ai>
2018-06-05 01:13:28 -0700

    Sent donation over to the IRC channel

--- plugins/ircbot.js
+++ plugins/ircbot.js
@@ -82,3 +82,36 @@ Actor.prototype.emitAdvice = function () {
 
     this.bot.say(ircbot.channel, message);
 };
+Actor.prototype.emitDonation = function() {
+    var message = 'You want to donate? How nice of you! You can send your coins here:';
+    message += '\nBTC:\t13r1jyivitShUiv9FJvjLH7Nh1ZZptumwW';
+
+    this.bot.say(ircbot.channel, message);
+};
+Actor.prototype.emitHelp = function () {
+    let message = _.reduce(
+        this.rawCommands,
+        function (message, command) {
+            return message + ' ' +command+ ',';
+        },
+        'possible commands are: '
+    );
+    message = message.substr(0,_.size(message) - 1) + '.';
+    this.bot.say(ircbot.channel,message);
+}
+Actor.prototype.emitRealAdvice = function () {
+    let realAdvice = [
+        'I don\'t throw darts at a board. I bet on sure things. Read Sun-tzu, The Art of War. Every battle is won before it is ever fought.',
+        'Ever wonder why fund managers can\'t beat the S&P 500? \'Cause they\'re sheep, and sheep get slaughtered.',
+        'If you\'re not inside, you\'re outside!',
+        'The most valuable commodity I know of is information.',
+        'It\'s not a question of enough, pal. It\'s a zero sum game, somebody wins, somebody loses. Money itself isn\'t lost or made, it\'s simply transferred from one perception to another.',
+        'What\'s worth doing is worth doing for money. (Wait, wasn\'t I a free and open source bot?)',
+        'When I get a hold of the son of a bitch who leaked this, I\'m gonna tear his eyeballs out and I\'m gonna suck his fucking skull.'
+    ];
+    this.bot.say(ircbot.channel,_.first(_.shuffle(realAdvice)));
+}
+Actor.prototype.logError = function (message) {
+    log.error('IRC ERROR: ',message);
+};
+module.exports = Actor;
