96c4ce2bb5a98cc84fb778be9622840ff55f3a32
kunal-PrimeTrade <kunal@primetrade.ai>
2018-06-05 12:31:56 +0530

    revised updated kraken market

--- util/genMarketFiles/update-kraken.js
+++ util/genMarketFiles/update-kraken.js
@@ -33,9 +33,6 @@ let getMinTradesSize = asset=>{
       case 'XICN':
           minTradeSize = '2.0';
           break;
-      case 'XLTC':
-          minTradeSize = '0.1';
-          break;
       case 'XMLN':
           minTradeSize = '0.1';
           break;
@@ -71,5 +68,63 @@ let assetPromise = request({
     },
     json: true,
 }).then(body=>{
-    
-})
+    if (!body || !body.result) {
+        throw new Error('Unable to fetch list of assets, response was empty')
+    } else if (!_.isEmpty(body.error)) {
+        throw new Error(`Unable to fetch list of assets: ${body.error}`);
+    }
+
+    return body.result;
+});
+
+let assetPairsPromise = request({
+    url: 'https://api.kraken.com/0/public/AssetPairs',
+    headers: {
+        Connection: 'keep-alive',
+        'User-Agent': 'Request-Promise',
+    },
+    json: true,
+}).then(body => {
+    if (!body || !body.result) {
+        throw new Error('Unable to fetch list of assets, response was empty')
+    } else if (!_.isEmpty(body.error)) {
+        throw new Error(`Unable to fetch list of assets: ${body.error}`);
+    }
+
+    return body.result;
+});
+
+Promise.all([assetPromise, assetPairsPromise])
+    .then(results => {
+        let assets = _.unique(_.map(results[1], market => {
+            return results[0][market.base].altname;
+        }));
+        let currencies = _.unique(_.map(results[1], market => {
+            return results[0][market.quote].altname;
+        }));
+        let marketKeys = _.filter(_.keys(results[1]), k => { return !k.endsWith('.d'); });
+        let markets = _.map(marketKeys, k => {
+            let market = results[1][k];
+            let asset = results[0][market.base];
+            let currency = results[0][market.quote];
+            return {
+                pair: [currency.altname, asset.altname],
+                prefixed: [market.quote, market.base],
+                book: k,
+                minimalOrder: {
+                    amount: getMinTradeSize(market.base),
+                    unit: 'asset',
+                },
+                precision: market.pair_decimals
+            };
+        });
+        return { assets: assets, currencies: currencies, markets: markets };
+    })
+    .then(markets => {
+        fs.writeFileSync('../../exchanges/kraken-markets.json', JSON.stringify(markets, null, 2));
+        console.log(`Done writing Kraken market data`);
+    })
+    .catch(err => {
+        console.log(`Couldn't import products from Kraken`);
+        console.log(err);
+    });
